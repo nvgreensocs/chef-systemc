fb7d674c041dbc17e5a3a65343f08a089e9f5830
Mark Burton <mark@greensocs.com>
2012-11-14 22:09:54 +0100

    stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,15 +4,17 @@ Chef::Log.warn("Assuming you have downloaded the systemc kit")
 bash "Extract systemc" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
-  
-echo "HERE"
-pwd
-    file chef-solo-1/cookbooks/chef-systemc/files/default/systemc-2.3.0.tgz
+ 
+  tar -xf chef-solo-1/cookbooks/chef-systemc/files/default/systemc-2.3.0.tgz
+  cd systemc-2.3.0
+  mkdir objdir
+  cd objdir
+  mkdir -p /usr/local/systemc-2.3.0
+   ../configure --prefix=/usr/local/systemc-2.3.0
+   make
+   make install
 
-     tar -zxf chef-solo-1/cookbooks/chef-systemc/files/default/systemc-2.3.0.tgz
-exit -2
-    
   EOH
-  creates "/usr/lib/systemc.so"
+  creates "/usr/local/systemc-2.3.0/lib/systemc.so"
 end
 
