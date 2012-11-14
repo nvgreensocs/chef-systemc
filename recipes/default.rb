d22d88e21fd3869905b27f35ab81cd240e48a01c
Mark Burton <mark@greensocs.com>
2012-11-14 21:02:41 +0100

    stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,21 @@
-package "systemc"
+Chef::Log.warn("Assuming you have downloaded the systemc kit")
+
+#cookbook_file "/vagrant/ModelLibrary/systemc-2.3.0.tgz" do
+#  source "systemc-2.3.0.tgz"
+#  mode "0644"
+#end
+
+
+bash "Extract systemc" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+
+echo "HERE"
+pwd
+
+     tar -zxf systemc-2.3.0.tgz
+    
+  EOH
+  creates "/vagrant/ModelLibrary/amba_socket"
+end
+
