c2244c0f80cdf62a1491897c32030b7342d8426f
Mark Burton <mark@greensocs.com>
2012-11-14 21:47:09 +0100

    stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,17 @@
 Chef::Log.warn("Assuming you have downloaded the systemc kit")
 
 
-tar_package 'systemc-2.3.0.tgz' do
-  prefix '/usr/local'
-  creates '/usr/local/lib/systemc.so'
+bash "Extract systemc" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+  
+echo "HERE"
+pwd
+
+     tar -zxf /tmp/systemc-2.3.0.tgz
+exit -2
+    
+  EOH
+  creates "/usr/lib/systemc.so"
 end
 
