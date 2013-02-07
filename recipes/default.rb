37dd4aa710bc25e2ac8bce89184ff9baf62d7288
Mark Burton <mark@greensocs.com>
2013-02-07 12:50:47 +0100

    updates to make the bash.profile work

--- recipes/default.rb
+++ recipes/default.rb
@@ -48,11 +48,12 @@ bash "Extract systemc" do
     echo $prefix/lib-linux >> /etc/ld.so.conf.d/systemc.conf
    fi
    mkdir -p "#{node[:prefix]}/bash.profile.d"
-   echo 'export LD_LIBRARY_PATH=$prefix/lib-linux64:$LD_LIBRARY_PATH' > "#{node[:prefix]}/bash.profile.d/systemc.profile"
-   echo 'export LD_LIBRARY_PATH=$prefix/lib-linux:$LD_LIBRARY_PATH' >> "#{node[:prefix]}/bash.profile.d/systemc.profile"
-
+   echo "export LD_LIBRARY_PATH=$prefix/lib-linux64:\$LD_LIBRARY_PATH" > "#{node[:prefix]}/bash.profile.d/systemc.profile"
+   echo "export LD_LIBRARY_PATH=$prefix/lib-linux:\$LD_LIBRARY_PATH" >> "#{node[:prefix]}/bash.profile.d/systemc.profile"
+   echo "export SYSTEMC_HOME=$prefix" >> "#{node[:prefix]}/bash.profile.d/systemc.profile"
+   echo "export CFLAGS=\"-I$prefix/include \$CFLAGS\"" >> "#{node[:prefix]}/bash.profile.d/systemc.profile"
   EOH
-  creates "/usr/local/systemc-2.3.0/README"
+  creates "#{node[:prefix]}/bash.profile.d/systemc.profile"
 end
 
 
